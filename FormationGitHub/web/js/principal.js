3f602202ee85258766f140db454920091f13b134
mickaelcm <>
2019-03-05 10:09:38 -0500

    Rajout du javascript
    On rajoute du code pour gérer la réponse à la question.

---
+++ FormationGitHub/web/js/principal.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function ClickSurBouton(valeur)
+{
+    if (valeur === 'Oui')
+    {
+        alert("Aller sur le site : https://openclassrooms.com/fr/courses/2342361-gerez-votre-code-avec-git-et-github");
+    }
+    else
+    {
+        alert("Dommage!!");
+    }
+}
+
